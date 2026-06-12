a2b11582a21dd3bb7c9f2468a862099ab81aefc4
Jesus Perez <jesusaperezs.05@gmail.comgit config --global user.email jesusaperezs.05@gmail.com>
2026-06-11 19:35:40 -0600

    feat: agregar endpoint POST /asteroides/fetch

--- api.py
+++ api.py
@@ -3,6 +3,8 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+from datetime import datetime, timedelta
 
 app = FastAPI(title="AsteroidesAPI", version="1.0")
 
@@ -46,3 +48,31 @@ def get_asteroide(id: int):
 @app.get("/")
 def health():
     return {"Status": "API Funcionando correctamente"}
+
+@app.post("/asteroides/fetch")
+def call_NASA():
+     
+     hoy = datetime.now().strftime("%Y-%m-%d")
+     mañana = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+
+     API_KEY = os.getenv("NASA_API_KEY")
+
+     url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={hoy}&end_date={mañana}&api_key={API_KEY}"
+     response = requests.get(url)
+     datos = response.json()
+
+     conn = get_bd_connection()    # ← antes del loop
+     cursor = conn.cursor()
+
+     for fecha, asteroides in datos["near_earth_objects"].items():
+        print(f"fecha: {len(asteroides)} asteroides")
+        for ast in asteroides[:3]:
+             nombre = ast["name"]
+             tamaño = ast["estimated_diameter"]["kilometers"]["estimated_diameter_min"]
+             insertar = "INSERT INTO asteroides (nombre, tamaño_km, fecha_deteccion, peligroso) VALUES (%s, %s, %s, %s)"
+             cursor.execute(insertar, (nombre, tamaño, fecha, True))
+             print(f" - nombre: {nombre}, tamaño: {tamaño:.2f} km")
+     conn.commit()
+     cursor.close()
+     conn.close()
+     return {"mensaje": "Asteroides insertados correctamente"}
